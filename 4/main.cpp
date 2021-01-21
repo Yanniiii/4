f750ad255e3badeae6c24003ee16d0c87b9f6576
FengHuangQing <851117557@qq.com>
2021-01-21 18:02:56 +0800

    1111

---
+++ 4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  4
+//
+//  Created by FengHuangQing on 2021/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World2!\n";
+    return 0;
+}
